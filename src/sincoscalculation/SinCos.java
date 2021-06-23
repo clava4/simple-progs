fc62931218af7ec5e10c18f4fb547886882035ee
clava4 <slavik.kulynych@gmail.com>
2021-06-23 22:22:12 +0300

    Refactored SinCos, added new project - CaesarCipher.

--- src/sincoscalculation/SinCos.java
+++ src/sincoscalculation/SinCos.java
@@ -5,53 +5,61 @@ public class SinCos {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         System.out.println("1 - sin(x)");
         System.out.println("2 - cos(x)");
-        int choice1 = in.nextInt();
+        int sinCosChoice = in.nextInt();
 
         System.out.println("1 - radian");
         System.out.println("2 - degree");
-        int choice2 = in.nextInt();
+        int systemChoice = in.nextInt();
 
         System.out.print("x = ");
         double argument = in.nextDouble();
+        double sum;
 
-        int koef = 1;
+        sum = (sinCosChoice == 1) ? sin(argumentChange(argument,systemChoice)) : cos(argumentChange(argument,systemChoice));
+
+        System.out.println(sum);
+    }
+
+    public static double argumentChange(double argument, int systemChoice){
+        int coefficient = 1;
         final double PI = 3.141592653;
 
-        if (choice2 == 2){
-            argument = argument * PI / 180;
+        if (systemChoice == 2){
+            argument *= PI / 180;
         }
 
         if (argument < 0){
-            koef = -1;
+            coefficient = -1;
         }
 
         while (Math.abs(argument) > 2 * PI){
-            argument = argument - 2 * PI * koef;
+            argument -= 2 * PI * coefficient;
         }
+        return argument;
+    }
 
-        double addition;
+    public static double sin(double argument){
         double sum;
-
-        if (choice1 == 1) {        //sin(x)
-            addition = argument;
-            sum = argument;
-            for (int j = 1; j < 50; j++) {
-                addition = - addition * argument * argument / (2 * j) / (2 * j + 1);
-                sum = sum + addition;
-            }
-
-        } else {                  //cos(x)
-            addition = 1;
-            sum = 1;
-            for (int j = 1; j < 50; j++) {
-                addition = - addition * argument * argument / (2 * j) / (2 * j - 1);
-                sum = sum + addition;
-            }
-
+        double addition = argument;
+        sum = argument;
+        for (int j = 1; j < 50; j++) {
+            addition = -addition * argument * argument / (2 * j) / (2 * j + 1);
+            sum = sum + addition;
         }
-        System.out.println(sum);
+        return sum;
+    }
+
+    public static double cos(double argument) {
+        double sum;
+        double addition = 1;
+        sum = 1;
+
+        for (int j = 1; j < 50; j++) {
+            addition = - addition * argument * argument / (2 * j) / (2 * j - 1);
+            sum = sum + addition;
+        }
+        return sum;
     }
 }
